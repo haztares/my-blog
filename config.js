0b409ca9716eb20a167301b44f51aaf0cf2b396b
Haztares Nguyen <mynt@dgroup.co>
2020-02-22 17:07:00 +0700

    Change template

---
+++ config.js
@@ -0,0 +1,49 @@
+"use strict";
+
+module.exports = {
+  url: "https://haztares.com",
+  pathPrefix: "/",
+  title: "Blog by Haztares Nguyen",
+  subtitle: "Fullstack developer",
+  copyright: "© All rights reserved.",
+  disqusShortname: "",
+  postsPerPage: 4,
+  googleAnalyticsId: "UA-73379983-2",
+  useKatex: false,
+  menu: [
+    {
+      label: "Articles",
+      path: "/"
+    },
+    {
+      label: "About me",
+      path: "/pages/about"
+    },
+    {
+      label: "Contact me",
+      path: "/pages/contacts"
+    }
+  ],
+  author: {
+    name: "Haztares Nguyen",
+    photo: "/avatar.jpg",
+    bio: "Fullstack developer.",
+    contacts: {
+      email: "",
+      facebook: "#",
+      telegram: "#",
+      twitter: "#",
+      github: "#",
+      rss: "",
+      vkontakte: "",
+      linkedin: "#",
+      instagram: "#",
+      line: "",
+      gitlab: "",
+      weibo: "",
+      codepen: "",
+      youtube: "",
+      soundcloud: ""
+    }
+  }
+};
